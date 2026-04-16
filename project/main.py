ec6b081e4c89f885559169bdac6b48bed5704b6a
Gandesri Abhilash <abhigandesri@gmail.com>
2026-04-17 00:37:57 +0530

    Add files via upload

--- project/main.py
+++ project/main.py
@@ -1,42 +1,41 @@
-from dotenv import load_dotenv
-load_dotenv()
+import os
 
-from langchain_google_genai import ChatGoogleGenerativeAI
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = "your_api_key"
+os.environ["LANGCHAIN_PROJECT"] = "My First App"
+
 from prompts.match_prompt import match_prompt
 from prompts.score_prompt import score_prompt
 from prompts.explain_prompt import explain_prompt
 from chains.extract_chain import extract_chain
+from langchain_openai import ChatOpenAI
 
-# Gemini model
-llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash-latest",   
-    temperature=0.3,
-    google_api_key=""
+
+llm = ChatOpenAI(
+    model="openai/gpt-3.5-turbo",
+    openai_api_key="your_api_key",
+    openai_api_base="https://openrouter.ai/api/v1"
 )
-def run_pipeline(resume, job):
 
-    # Step 1: Extract
-    extracted = extract_chain(llm, resume)
+def run_pipeline(resume, job, name):
 
-    # Step 2: Match
-    match = (match_prompt | llm).invoke({
-        "resume_data": extracted.content,
-        "job": job
-    })
+        extracted = extract_chain(llm, resume)
 
-    # Step 3: Score
-    score = (score_prompt | llm).invoke({
-        "match": match.content
-    })
+        match = (match_prompt | llm).invoke({
+            "resume_data": extracted.content,
+            "job": job
+        })
 
-    # Step 4: Explain
-    explanation = (explain_prompt | llm).invoke({
-        "score": score.content,
-        "match": match.content
-    })
+        score = (score_prompt | llm).invoke({
+            "match": match.content
+        })
 
-    return score.content, explanation.content
+        explanation = (explain_prompt | llm).invoke({
+            "score": score.content,
+            "match": match.content
+        })
 
+        return score.content, explanation.content
 
 # Load job
 with open("job_description.txt") as f:
@@ -49,7 +48,7 @@ for file in files:
     with open(f"resumes/{file}") as f:
         resume = f.read()
 
-    score, explanation = run_pipeline(resume, job)
+    score, explanation = run_pipeline(resume, job, file)
 
     print("\n====================")
     print("Resume:", file)
